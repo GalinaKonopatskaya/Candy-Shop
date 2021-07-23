82269a100fb2a0310392bbf80c43532f608244e4
Galina <79738437+GalinaKonopatskaya@users.noreply.github.com>
2021-07-23 11:49:24 +0300

    Delete 2021 Pro directory

--- 2021 Pro/cl_w/utils.js
+++
@@ -1,9 +0,0 @@
-function getUrl(baseUrl) {
-    //getCategory from LS
-    let url = baseUrl;
-    let category = localStorage.getItem("optionValue");
-    if (category) {
-        url += `?category=${category}`;
-    }
-    return url
-}
